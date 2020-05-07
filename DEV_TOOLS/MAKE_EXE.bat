04822c343c5277153599d20e3581d3e89e5c0cfe
Joshua Merold <merold.work@gmail.com>
2020-05-07 11:26:54 +0200

    Delete MAKE_EXE.bat

--- DEV_TOOLS/MAKE_EXE.bat
+++
@@ -1,8 +0,0 @@
-pyinstaller.exe --onefile --console -w -i "%~dp0..\qw_logo.ico" %~dp0..\main.py
-copy %~dp0dist\main.exe ..\
-copy %~dp0main.spec ..\
-Xcopy /E %~dp0build %~dp0..\
-rmdir /Q/S dist
-rmdir /Q/S build
-del /f/Q main.spec
-pause
